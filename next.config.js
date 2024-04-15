7fec1e2acdb04d64e28721e492ee0437ac4c9741
Ahmed-Shayan-Arsalan <shayanarsalan003@gmail.com>
2024-04-15 21:34:31 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output:'export',
-    //basePath:'/Portfolio',
-    basePath:'',
+    output: "export",  // <=== enables static exports
+    basePath:"/portfolio",
+    //basePath:'',
 };
 
 export default nextConfig;
