532246ee74228fde500072d2c051cb4071eff138
Shayan <141062129+Ahmed-Shayan-Arsalan@users.noreply.github.com>
2025-03-30 23:43:23 +0500

    Update aboutData.js

--- src/Data/aboutData.js
+++ src/Data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData={
     name:"Ahmed Shayan",
-    desc:"I'm A Data Scientist That Dives In Data Analysis, Visulizations, And AI Modules.",
+    desc:"I'm A Data Scientist That Dives In Data Analysis, Visulizations, And AI Modules. I work directly with LLMs, Automations, and making scalable solutions",
     based:"Islamabad, Pakistan",
 }
